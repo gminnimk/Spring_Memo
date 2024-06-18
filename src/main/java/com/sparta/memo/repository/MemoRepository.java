6a5da2b88ae91bfee5da6b07473b87f5a5fb2895
gminnimk <tvt1245@naver.com>
2024-06-18 15:36:06 +0900

    Spring 입문주차 2주차 (2-14) : Query Method

--- src/main/java/com/sparta/memo/repository/MemoRepository.java
+++ src/main/java/com/sparta/memo/repository/MemoRepository.java
@@ -1,13 +1,13 @@
 package com.sparta.memo.repository;
 
 import com.sparta.memo.entity.Memo; // Memo 엔티티를 import
+import org.springframework.data.jpa.repository.JpaRepository; // Spring Data JPA의 JpaRepository를 import
 
-import org.springframework.data.jpa.repository.JpaRepository; // JpaRepository 인터페이스를 import
+import java.util.List; // List 클래스를 import
 
-/**
- * Memo 엔티티를 관리하기 위한 Spring Data JPA Repository 인터페이스입니다.
- * JpaRepository를 상속받아 Memo 엔티티에 대한 기본적인 CRUD 작업을 자동으로 처리합니다.
- */
+// JpaRepository를 확장하여 Memo 엔티티의 리포지토리를 정의하는 인터페이스
 public interface MemoRepository extends JpaRepository<Memo, Long> {
 
+    // modifiedAt 필드를 기준으로 내림차순으로 모든 Memo 엔티티를 조회하는 메서드
+    List<Memo> findAllByOrderByModifiedAtDesc();
 }
